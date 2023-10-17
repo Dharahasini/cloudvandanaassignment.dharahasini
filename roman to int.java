5b629ac9c90b80e68a618ed86c15fe3da38832ab
Dharahasini <101558960+Dharahasini@users.noreply.github.com>
2023-10-17 11:34:17 +0530

    Create roman to int.java

---
+++ roman to int.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine();
+        scanner.close();
+
+        int result = romanToInt(romanNumeral);
+        if (result != 0) {
+            System.out.println("Integer value: " + result);
+        } else {
+            System.out.println("Invalid Roman numeral entered.");
+        }
+    }
+
+    public static int romanToInt(String s) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int value = romanCharToInt(c);
+
+            if (value == 0) {
+                return 0;  
+            }
+
+            if (value < prevValue) {
+                result -= value;
+            } else {
+                result += value;
+            }
+
+            prevValue = value;
+        }
+
+        return result;
+    }
+
+    public static int romanCharToInt(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;  
+        }
+    }
+}
